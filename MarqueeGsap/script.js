2d7aa9c4fb95abe71286d004e44f7178b126c401
MakandarSahil <sahilmakandar15@gmail.com>
2025-02-08 23:57:34 +0530

    Marquee using gsap

---
+++ MarqueeGsap/script.js
@@ -0,0 +1,46 @@
+let marqueeAnimation = () => {
+  let isAnimating = false;
+
+  window.addEventListener("wheel", (event) => {
+    if (isAnimating) return;
+    isAnimating = true;
+
+    if (event.deltaY > 0) {
+      gsap.to(".marquee", {
+        transform: "translateX(-200%)",
+        repeat: -1,
+        duration: 4,
+        ease: "none",
+        onComplete: () => {
+          isAnimating = false;
+        }
+      });
+
+      gsap.to(".marquee i", {
+        rotate: 180,
+        onComplete: () => {
+          isAnimating = false;
+        }
+      });
+    } else {
+      gsap.to(".marquee", {
+        transform: "translateX(0%)",
+        repeat: -1,
+        duration: 4,
+        ease: "power2.out",
+        onComplete: () => {
+          isAnimating = false;
+        }
+      });
+
+      gsap.to(".marquee i", {
+        rotate: 0,
+        onComplete: () => {
+          isAnimating = false;
+        }
+      });
+    }
+  });
+};
+
+marqueeAnimation();
